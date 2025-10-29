2c757d3cb08e639b46cfd35d2564698a10d109d5
lochinbek9 <lochinbeksetor@gmail.com>
2025-10-29 21:06:35 +0500

    good

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
 
 import './App.css'
 import HomePage from './pages/Home/HomePage';
 
 
 function App() {
- 
-
-  console.log("Hello");
-  
   return (
     <>
      <HomePage/>
